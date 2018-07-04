b2e0c23bc3a0a4e4d255d83b2739331f92e46a39
Navnath Sujgure <navnath.sujgure@springernature.com>
2018-07-04 11:56:22 +0530

    my first commit

---
+++ src/main/java/com/crm/qa/base/TestBase.java
@@ -0,0 +1,21 @@
+package com.crm.qa.base;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class TestBase 
+{
+
+	static Properties prop = new Properties();
+	
+	TestBase()
+	{
+		
+		
+		
+		
+	}
+	
+	
+}
